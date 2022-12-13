1a1144c43310e3a8798153e8508bac0aba56c4e3
lichengwu <lichengwu@foxmail.com>
2022-12-13 23:26:27 +0800

    完成部分项目属性填充。

--- src/views/project/components/project-detail.vue
+++ src/views/project/components/project-detail.vue
@@ -1,16 +1,87 @@
 <template>
   <!-- 项目详情 -->
   <div class="app-container">
-    <el-form ref="postForm" :loading="loading" :model="postForm" :rules="rules" label-width="80px">
-      <el-form-item label="id" v-if="false" prop="id">
-        <el-input v-model="postForm.id"></el-input>
-      </el-form-item>
-      <el-form-item label="项目名称" prop="name">
-        <el-input v-model="postForm.name"></el-input>
-      </el-form-item>
-      <el-form-item label="描述" prop="description">
-        <el-input v-model="postForm.description"></el-input>
-      </el-form-item>
+    <el-form ref="postForm" :loading="loading" :model="postForm" :rules="rules" label-width="100px">
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="id" v-if="false" prop="id">
+            <el-input v-model="postForm.id"></el-input>
+          </el-form-item>
+          <el-form-item label="名称" prop="name">
+            <el-input v-model="postForm.name"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="编号" prop="code">
+            <el-input v-model="postForm.code"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="承担部门" prop="departmentName">
+            <el-input v-model="postForm.departmentName"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="类型" prop="projectTypeName">
+            <el-input v-model="postForm.projectTypeName"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="负责人" prop="managerName">
+            <el-input v-model="postForm.managerName"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="开始时间" prop="startFormat">
+            <el-input v-model="postForm.startFormat"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="结束时间" prop="completionFormat">
+            <el-input v-model="postForm.completionFormat"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="难易程度" prop="importanceName">
+            <el-input v-model="postForm.importanceName"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="城市" prop="cityName">
+            <el-input v-model="postForm.cityName"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="区县" prop="county">
+            <el-input v-model="postForm.county"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="乡镇" prop="town">
+            <el-input v-model="postForm.town"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="甲方名称" prop="partA">
+            <el-input v-model="postForm.partA"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="甲方联系人" prop="partAContact">
+            <el-input v-model="postForm.partAContact"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="甲方电话" prop="partAPhone">
+            <el-input v-model="postForm.partAPhone"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="24">
+          <el-form-item label="项目评价" prop="evaluation">
+            <el-input v-model="postForm.evaluation"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
     </el-form>
     <span slot="footer" class="dialog-footer">
       <el-button type="primary" @click="submit">提交更新</el-button>
@@ -31,6 +102,19 @@ export default {
       postForm: {
         id: '', //当前项目的id，也是路由入参
         name: '',
+        code: '',
+        projectTypeName: '',
+        startFormat: '',
+        completionFormat: '',
+        departmentName: '',
+        managerName: '',
+        importanceName: '',
+        cityName: '',
+        county: '',
+        town: '',
+        partA: '',
+        partAContact: '',
+        partAPhone: '',
       },
       dialogVisible: false,
       rules: {
@@ -91,6 +175,7 @@ export default {
       fetchProjectDetail(this.postForm.id).then((res) => {
         console.log(res.data)
         this.postForm = res.data.project
+        this.postForm.managerName = res.data.projectManager
         this.loading = false
       }).catch((err) => {
         this.$message({
